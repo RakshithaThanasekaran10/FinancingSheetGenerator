0f9b86af73a2ae4eaa865cc6c2ff138a920e9173
petalbloom <ga3menon@gmail.com>
2026-03-09 16:05:08 -0400

    Did some error fixing, made more edits for creating basic pdf generation implementation

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("generate_pdf/", views.generate_pdf, name="generate_pdf"),
 ] 
--- main/views.py
+++ main/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from xhtml2pdf import pisa
 
 def home(request):
     """
     Homepage view: renders the skeleton of the finance sheet generator
     """
     return render(request, "main/home.html") 
+
+def generate_pdf(request):
+
+    if request.method == "POST":
+
+        income = float(request.POST.get("income", 0))
+        expenses = float(request.POST.get("expenses", 0))
+        balance = income - expenses
+
+        html_string = render_to_string(
+            "pdf_template.html",
+            {
+                "income": income,
+                "expenses": expenses,
+                "balance": balance
+            }
+        )
+
+        pdf = HTML(string=html_string).write_pdf()
+
+        response = HttpResponse(pdf, content_type="application/pdf")
+        response["Content-Disposition"] = "inline; filename=finance_sheet.pdf"
+
+        return response
