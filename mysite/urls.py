23ed3a39d09330dcac2b0ba826d81618c86c6c71
Joey <brs4bt@virginia.edu>
2026-03-09 16:30:14 -0400

    Added generate_pdf to urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,4 +5,5 @@ from mysite import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path("generate_pdf/", views.generate_pdf, name='generate_pdf'),
 ] 
