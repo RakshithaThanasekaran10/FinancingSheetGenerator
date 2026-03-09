daea3f548859c1fe8b8dc16b6d64be8b6ec0269b
Joey <brs4bt@virginia.edu>
2026-03-09 16:27:48 -0400

    fixed urls and views not being in the same folder, merged pdf branch into this one.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from mysite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # includes main app urls
+    path('', views.home, name='home'),
 ] 
